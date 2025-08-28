99cb8ca7ddce3a0dcd2e22326446da327384adf9
BenoitV <Benoitvicaireformation@gmail.com>
2025-08-28 20:13:52 +0200

    lion mort

--- LVL1/lion.py
+++ LVL1/lion.py
@@ -3,30 +3,29 @@ import string
 abc=list(string.ascii_uppercase)
 divinite ="ARTEMIS ASCLEPIOS ATHENA ATLAS CHARON CHIRON CRONOS DEMETER EOS ERIS EROS GAIA HADES HECATE HEPHAISTOS HERA HERMES HESTIA HYGIE LETO MAIA METIS MNEMOSYNE NYX OCEANOS OURANOS PAN PERSEPHONE POSEIDON RHADAMANTHE SELENE THEMIS THETIS TRITON ZEUS"
 listdiv = divinite.split(" ")
-dictionarie_word_value={}
+# dictionarie_word_value={}
+list_word_value=[]
 
 # listdiv[i] retourne l'item i de listdiv, donc un mot entier
 # listdiv[i][j] retourne l'item j de listdiv[i] : donc une lettre
 
-for i in range (0,len(listdiv)):
+for i in range (len(listdiv)):
     word=listdiv[i]
     word_value=0
-    for j in range (0,len(word)):       
+    for j in range (len(word)):       
         word_value=word_value+abc.index(word[j])+1
-        dictionarie_word_value[listdiv[i]]=word_value
-        # print(word_value)
-                
-# print(dictionarie_word_value)
-swapkey = ""
-swapvalue = 0
-# for i in dictionarie_word_value.items():
-#     # if i[1] < (i-1)[1] :
-#     #     swapkey = i[0]
-#     #     swapvalue=i[1]
-#     #     dictionarie_word_value.update(i)
-#     print(i[1])
+    temp_tuple=(listdiv[i],word_value)
+    list_word_value.append(temp_tuple)
 
-        # dictionarie_word_value.pop(i)
-    # print(i)
-    # print(i[1])
-print(dictionarie_word_value.items())   
+
+# print(list_word_value)
+# for i in range (len(list_word_value)**5):
+#     if list_word_value[i%34][1] < list_word_value[i%34+1][1]:
+#         list_word_value[i%34],list_word_value[i%34-1] = list_word_value[i%34-1],list_word_value[i%34]
+# print(list_word_value)
+
+list_word_value.sort(key=lambda a: a[1])
+solution=""
+for i in range (len(list_word_value)):
+    solution=solution+" " +list_word_value[i][0]
+print(solution)
---
+++ LVL1/test.py
@@ -0,0 +1,8 @@
+# list=[]
+# tuple=()
+# dictionaire={}
+maListe=[]
+montuple=("clé","valeur")
+print(montuple[0])
+maListe.append(montuple)
+print(maListe[0][1])
