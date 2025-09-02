a2440d3f2d4e0d7f8a6a14f7794372a769aac975
BenoitVicaire <Benoitvicaireformation@gmail.com>
2025-09-02 10:08:00 +0200

    started pokedex

--- LVL2/créature_nocturnes.py
+++ LVL2/créature_nocturnes.py
@@ -6,18 +6,47 @@ def time(seconde):
     hours = f"h:{heures} ,m:{minutes_restantes} ,s:{secondes_restantes}"
     return hours
 
-print(time(300))
+print(time(50*60))
 
 chauves_souris=0
 skellingtons=0
 zombies=0
 fantomes_baveux=0
+pU=0
+
+
 t=0
+
 while (t<(50*60)):
+    
+    pU=t//(4*60)
+    powerUp_cs=2*pU
+    powerUp_s=pU
+    powerUp_z=pU
+    powerUp_fb=pU
     if t%2==0:
         chauves_souris+=10
     if t%5==0:
         skellingtons+=5
     if t%6==0:
-        
+        zombies+=4
+    if t%10==0:
+        fantomes_baveux+=3 
+    if t%6==0:
+        chauves_souris-=2+powerUp_cs
+        chauves_souris=max(0,chauves_souris)
+    if t%20==0:
+        skellingtons-=1+powerUp_s
+        skellingtons=max(0,skellingtons)
+    if t%30==0:
+        zombies-=1+powerUp_z
+        zombies=max(0,zombies)
+    if t%40==0:
+        fantomes_baveux-=1+powerUp_fb
+        fantomes_baveux=max(0,fantomes_baveux)
     t+=1
+print(f"{chauves_souris}, {skellingtons}, {zombies}, {fantomes_baveux}")
+print(powerUp_cs,powerUp_s,powerUp_z,powerUp_fb)
+    
+
+    
